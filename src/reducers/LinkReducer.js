e8fc24774415dfc60e187c910b5f666c132a65d7
Caio Alencikas <caio.alencikas@gmail.com>
2020-07-12 18:58:13 -0300

    Links listing

--- src/reducers/LinkReducer.js
+++ src/reducers/LinkReducer.js
@@ -1,8 +1,9 @@
-import { LINK_CREATE } from '../actions/LinkActions'
+import { LINK_CREATE, LINK_LIST} from '../actions/LinkActions'
 
 
 const initialState = {
     link: null,
+    links: [],
 }
 
 export default function (state = initialState, action) {
@@ -12,6 +13,11 @@ export default function (state = initialState, action) {
             const response = payload ? payload.data : null
             const link = response ? response.data : null
             return {...state, link}
+        }
+        case LINK_LIST: {
+            const response = payload ? payload.data : null
+            const links = response ? response.data : null
+            return {...state, links}
         }
         default: {
             return state
