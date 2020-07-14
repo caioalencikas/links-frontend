e7f72f769a34a11f31be357ebbf80deefa65e62e
Caio Alencikas <caio.alencikas@gmail.com>
2020-07-14 18:34:06 -0300

    Started delete process

--- src/screens/Manage/Links/index.js
+++ src/screens/Manage/Links/index.js
@@ -1,17 +1,17 @@
 import React, { useEffect } from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import Layout from '../../Layouts/Manage'
-import {linkList} from '../../../actions/LinkActions'
+import { linkList, setLinkToRemove } from '../../../actions/LinkActions'
 
-const Links = ({links, linkList}) => {
+const Links = ({ links, linkList, setLinkToRemove, linkToRemove }) => {
 
     useEffect(() => {
         linkList()
     }, [linkList])
 
-    return ( 
+    return (
         <Layout>
             <div className="row">
                 <div className="col">
@@ -26,32 +26,42 @@ const Links = ({links, linkList}) => {
 
             {links && links.length
                 ? links.map((link) => {
+
+                    const deleteClick = (e) => setLinkToRemove(link)
+
+                    const border = (linkToRemove && linkToRemove.id === link.id) ? 'border border-danger rounded' : 'border border-transparent'
+
                     return (
-                        <div key={link.id} className="pb-2 pt-2 pl-3 pr-3 d-flex flex-row justify-content-between">
+                        <div key={link.id} className={`pb-2 pt-2 pl-3 pr-3 d-flex flex-row justify-content-between ${border}`}>
                             <div className="pr-3">
-                                <img src="https://via.placeholder.com/100" alt="Link icon"/>
+                                <img src="https://via.placeholder.com/100" alt="Link icon" />
                             </div>
                             <div className="align-self-center">
                                 <span className="text-primary clearfix">{link.label}</span>
                                 <span className="text-primary clearfix">{link.url}</span>
                             </div>
                             <div className="ml-auto p-2 clearfix">
-                                <Link to= {`/manage/links/edit/${link.id}`} >
+                                <Link to={`/manage/links/edit/${link.id}`} >
                                     Edit
                                 </Link>
-                                <span>Delete</span>
+                                <button className="btn btn-clear" onClick={deleteClick}>
+                                    Delete
+                                </button>
                             </div>
-                        </div> 
+                        </div>
                     )
                 })
-            : null}
+                : null}
         </Layout>
     )
 }
 
 
 const mapStateToProps = (state) => {
-    return {links: state.link.links}
+    return {
+        links: state.link.links,
+        linkToRemove: state.link.linkToRemove,
+    }
 }
 
-export default connect(mapStateToProps, {linkList})(Links)
+export default connect(mapStateToProps, { linkList, setLinkToRemove })(Links)
