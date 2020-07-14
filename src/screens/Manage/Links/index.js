c4575c9e4839ab2cacd37a437415aa582935c6fe
Caio Alencikas <caio.alencikas@gmail.com>
2020-07-14 18:59:32 -0300

    delete implemented

--- src/screens/Manage/Links/index.js
+++ src/screens/Manage/Links/index.js
@@ -3,13 +3,16 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import Layout from '../../Layouts/Manage'
-import { linkList, setLinkToRemove } from '../../../actions/LinkActions'
+import { linkList, setLinkToRemove, linkRemove } from '../../../actions/LinkActions'
 
-const Links = ({ links, linkList, setLinkToRemove, linkToRemove }) => {
+const Links = ({ links, linkList, setLinkToRemove, linkToRemove, linkRemove }) => {
 
     useEffect(() => {
         linkList()
     }, [linkList])
+
+    const cancelDelete = (e) => setLinkToRemove(null)
+    const confirmDelete = (e) => (linkToRemove ? linkRemove(linkToRemove) : null)
 
     return (
         <Layout>
@@ -52,6 +55,18 @@ const Links = ({ links, linkList, setLinkToRemove, linkToRemove }) => {
                     )
                 })
                 : null}
+
+
+                {linkToRemove ? (
+                <div className="alert alert-danger rounded float-center shadow-bold">
+                    <h4 className="alert-heading">Delete Confirmation!</h4>
+                    <p>Are you sure you want to delete, this action cannot be undone.</p>
+                    <hr/>
+                    <div className="d-flex justify-content-between">
+                        <button className="btn btn-outline-light" onClick={cancelDelete}>cancel</button>
+                        <button className="btn btn-danger" onClick={confirmDelete}>delete</button>
+                    </div>
+                </div>) : null}
         </Layout>
     )
 }
@@ -64,4 +79,4 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps, { linkList, setLinkToRemove })(Links)
+export default connect(mapStateToProps, { linkList, setLinkToRemove, linkRemove })(Links)
